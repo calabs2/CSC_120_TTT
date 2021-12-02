bf58f655166a41ab15d831a8af86ca13bb4f6b26
Caleb <calabs2@yahoo.com>
2021-12-02 15:32:10 -0500

    Tic Tac Toe Program

--- game.py
+++ game.py
@@ -1,53 +1,86 @@
-print("player 1 : X" + " " + "player 2 : O")
-def game():
-	turn = "X"
-	count = 0
-	player_turn = player1
+import os    
+import time    
+    
+board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']    
+player = 1    
+   
+Win = 1    
+Draw = -1    
+Running = 0    
+Stop = 1    
 
-	for i in range(10) :
-		print(board)
-		print("It's your turn, " + player_turn + ". Where do you want to put your piece?"
-		move = eval(input())
+Game = Running    
+Mark = 'X'    
 
-		if board(move) == " ":
-			board(move) = turn
-			count += 1
-		else:
-			print("This place is already filled.\nMove to which place?"
-			continue
+def DrawBoard():    
+    print(" %c | %c | %c " % (board[1],board[2],board[3]))    
+    print("___|___|___")    
+    print(" %c | %c | %c " % (board[4],board[5],board[6]))    
+    print("___|___|___")    
+    print(" %c | %c | %c " % (board[7],board[8],board[9]))    
+    print("   |   |   ")    
 
-		if count >= 5:
-			if board[7] == board[8] == board[9] != " ":
-				declare_winner(turn)
-               		 	break
-            		elif board[4] == board[5] == board[6] != " ":  # across the middle
-                		declare_winner(turn)
-                		break
-         		elif board[1] == board[2] == board[3] != " ":  # across the bottom
-              			declare_winner(turn)
-               			break
-            		elif board[1] == board[4] == board[7] != " ":  # down the left side
-                		declare_winner(turn)
-                		break
-            		elif board[2] == board[5] == board[8] != " ":  # down the middle
-                		declare_winner(turn)
-                		break
-            		elif board[3] == board[6] == board[9] != " ":  # down the right side
-                		declare_winner(turn)
-                		break
-            		elif board[7] == board[5] == board[3] != " ":  # diagonal
-                		declare_winner(turn)
-                		break
-            		elif board[1] == board[5] == board[9] != " ":  # diagonal
-                		declare_winner(turn)
-                		break
-		if count == 9:
-			print("\nGame Over.\n")
-			print("It's a Tie!")
-
-		if turn == "X":
-			turn == "O"
-			player_turn = player2
-		else:
-		 	turn == "X"
-			player_turn = player1
+def CheckPosition(x):    
+    if(board[x] == ' '):    
+        return True    
+    else:    
+        return False    
+   
+def CheckWin():    
+    global Game    
+    #Horizontal conditions
+    if(board[1] == board[2] and board[2] == board[3] and board[1] != ' '):    
+        Game = Win    
+    elif(board[4] == board[5] and board[5] == board[6] and board[4] != ' '):    
+        Game = Win    
+    elif(board[7] == board[8] and board[8] == board[9] and board[7] != ' '):    
+        Game = Win    
+    #Vertical conditions 
+    elif(board[1] == board[4] and board[4] == board[7] and board[1] != ' '):    
+        Game = Win    
+    elif(board[2] == board[5] and board[5] == board[8] and board[2] != ' '):    
+        Game = Win    
+    elif(board[3] == board[6] and board[6] == board[9] and board[3] != ' '):    
+        Game=Win    
+    #Diagonal conditions  
+    elif(board[1] == board[5] and board[5] == board[9] and board[5] != ' '):    
+        Game = Win    
+    elif(board[3] == board[5] and board[5] == board[7] and board[5] != ' '):    
+        Game=Win    
+    #Tie or Draw conditions
+    elif(board[1]!=' ' and board[2]!=' ' and board[3]!=' ' and board[4]!=' ' and board[5]!=' ' and board[6]!=' ' and board[7]!=' ' and board[8]!=' ' and board[9]!=' '):    
+        Game=Draw    
+    else:            
+        Game=Running    
+    
+print("Tic-Tac-Toe Game Designed By Caleb Agosta")    
+print("Player 1 [X] --- Player 2 [O]\n")    
+print()    
+print()    
+print("Please Wait...")    
+time.sleep(3)    
+while(Game == Running):    
+    os.system('cls')    
+    DrawBoard()    
+    if(player % 2 != 0):    
+        print("Player 1's chance")    
+        Mark = 'X'    
+    else:    
+        print("Player 2's chance")    
+        Mark = 'O'    
+    choice = int(input("Enter the position between [1-9] where you want to mark : "))    
+    if(CheckPosition(choice)):    
+        board[choice] = Mark    
+        player+=1    
+        CheckWin()    
+    
+os.system('cls')    
+DrawBoard()    
+if(Game==Draw):    
+    print("Game Draw")    
+elif(Game==Win):    
+    player-=1    
+    if(player%2!=0):    
+        print("Player 1 Won")    
+    else:    
+        print("Player 2 Won")
